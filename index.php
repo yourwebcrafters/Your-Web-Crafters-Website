82c997ddeeeda4df1547b4065dcdfd97d0d95276
yourwebcrafters <brad@yourwebcrafters.com>
2015-11-23 11:56:28 -0500

    Setup New Base
    Setup the new base for the project.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Website Development Crafting SEO Promotion Creator Specialist</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link href='https://fonts.googleapis.com/css?family=Bangers|Russo+One|Lobster+Two|Courgette|Berkshire+Swash' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/app.css" />
+    <script src="js/vendor/modernizr.js"></script>
+
+  </head>
+  <body>
+  	<div class="row full-width"><!-- Main Web Page Wrap Creation Start -->
+
+
+    </div><!-- Main Row Website Creator Stop Page End -->
+
+		<script src="js/vendor/jquery.js"></script>
+		<script src="js/foundation.min.js"></script>
+		<script>
+			$(document).foundation();
+		</script>
+	</body>
+</html>
